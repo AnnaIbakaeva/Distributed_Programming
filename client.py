9b8c36a83cd805585402cb48226868d1c6cb8462
Анна Ибакаева <annaibakaeva@gmail.com>
2016-10-28 21:01:24 +0500

    Наработки по динамическому распределению нагрузки

--- client.py
+++ client.py
@@ -1,30 +1,50 @@
 import rpyc
+import time
 
-
-class ChatClient(object):
+class Information(object):
     def __init__(self):
+        self.received_data = []
+        self.client = None
+
+
+class Client(object):
+    def __init__(self):
+        # init values
         self.conn = None
         self.user = None
         self.name = "Kate"
-        self.text_input = ""
+        self.iterationsCount = 10000
+        self.iterations_size = 10
+
+        self.my_pi = []
+
         self.pi = 0
         self.received_pi = 0
-        self.rank = 0
-        self.n = 10000
-        self.size = 2
         self.result_pi = 0
+        self.messages_counter = 0
 
         self.on_connect()
 
-        for i in range(self.rank, self.n, self.size):
-            # print("")
-            # print("My pi = ",self.pi)
-            self.pi += pow(-1, i) / (2 * i + 1)
-            self.on_send()
+        self.size = self.user.get_clients_count()
+        self.rank = self.user.get_rank()
+
+        for i in range(0, self.iterationsCount):
+            self.calculate_pi()
+
+        # for i in range(self.rank, self.iterationsCount, self.size):
+        #     print("")
+        #     print("My pi = ",self.pi)
+        #     self.pi += pow(-1, i) / (2 * i + 1)
+        #     self.on_send()
 
         self.result_pi = self.pi + self.received_pi
         print("Result pi = ", self.result_pi * 4)
 
+        count = self.iterationsCount / self.size
+        while self.messages_counter < count:
+            time.sleep(10)
+
+        print(self.messages_counter, " = ", count)
         self.on_close()
 
 
@@ -40,6 +60,7 @@ class ChatClient(object):
             self.conn = None
 
     def on_close(self):
+        print("I closed!")
         self.disconnect()
 
     def on_connect(self):
@@ -51,53 +72,49 @@ class ChatClient(object):
             print("")
             self.conn = None
             return
-
         try:
-            self.user = self.conn.root.login(self.name, self.on_message)
+            self.user = self.conn.root.login(self.name, self.on_received)
         except ValueError:
             self.conn.close()
             self.conn = None
             return
 
+    def calculate_pi(self):
+        pi = 0
+        for i in range(0, self.iterations_size):
+            pi += pow(-1, i) / (2 * i + 1)
+        self.my_pi.append(pi)
+        # self.on_send()
 
-    #
-    # called by the reactor whenever the connection has something to say
-    #
-    def bg_server(self, source = None, cond = None):
-        if self.conn:
-            self.conn.poll_all()
-            return True
-        else:
-            return False
-
-    #
-    # sends the current message
-    #
     def on_send(self):
         text = self.pi
-        # print("")
-        # print("Send ", text)
+        print("")
+        print("Send ", text)
         self.user.say(text)
 
-    #
-    # called by the server, with the text to append to the GUI
-    #
-    def on_message(self, text):
-        # print("")
-        # print ("Received: ", text)
-        # print("My received pi = ", self.received_pi)
+    def on_received(self, text):
         mes = text.split(' ')
-        # print("My name is ", self.name)
         name = "[" + self.name + "]"
         if mes[0] == name:
-            # print("Oops, it's my pi!")
             return
+        print("")
+        print ("Received: ", text)
+        # print("My received pi = ", self.received_pi)
         self.received_pi = float(mes[1])
         # print("I summed receiving pi ", self.received_pi)
+        self.result_pi = self.pi + self.received_pi
+        # print("Result pi after receiving = ", self.result_pi * 4)
+        self.messages_counter += 1
+        print("Messages counter = ", self.messages_counter)
+
+    def update_iterations(self, unsolved):
+        activeClientsCount = self.user.get_active_clients_count()
+        deltaIteration = unsolved / activeClientsCount
+        self.iterationsCount += deltaIteration
 
 
 if __name__ == "__main__":
-    cc = ChatClient()
+    cc = Client()
 
 
 
--- client2.py
+++ client2.py
@@ -1,18 +1,18 @@
 import rpyc
-
+import time
 
 class ChatClient(object):
     def __init__(self):
         self.conn = None
         self.user = None
         self.name = "Ann"
-        self.text_input = ""
         self.pi = 0
         self.received_pi = 0
         self.rank = 1
         self.n = 10000
         self.size = 2
         self.result_pi = 0
+        self.messages_counter = 0
 
         self.on_connect()
 
@@ -25,7 +25,14 @@ class ChatClient(object):
         self.result_pi = self.pi + self.received_pi
         print("Result pi = ", self.result_pi * 4)
 
+        count = self.n / self.size
+
+        while self.messages_counter < count:
+            time.sleep(10)
+
+        print(self.messages_counter, " = ", count)
         self.on_close()
+
 
     def disconnect(self):
         if self.conn:
@@ -39,12 +46,16 @@ class ChatClient(object):
             self.conn = None
 
     def on_close(self):
+        print("I closed!")
         self.disconnect()
 
     def on_connect(self):
         try:
             self.conn = rpyc.connect("localhost", 19912)
         except Exception:
+            print("")
+            print("EXCEPTION!!!")
+            print("")
             self.conn = None
             return
 
@@ -71,24 +82,26 @@ class ChatClient(object):
     #
     def on_send(self):
         text = self.pi
-        # print("")
-        # print("Send ", text)
+        print("")
+        print("Send ", text)
         self.user.say(text)
 
     #
     # called by the server, with the text to append to the GUI
-    #
     def on_message(self, text):
-        # print("")
-        # print ("Received: ", text)
-        # print("My received pi = ", self.received_pi)
         mes = text.split(' ')
-        # print("My name is ", self.name)
         name = "[" + self.name + "]"
         if mes[0] == name:
             return
+        print("")
+        print ("Received: ", text)
+        # print("My received pi = ", self.received_pi)
         self.received_pi = float(mes[1])
         # print("I summed receiving pi ", self.received_pi)
+        self.result_pi = self.pi + self.received_pi
+        # print("Result pi after receiving = ", self.result_pi * 4)
+        self.messages_counter += 1
+        print("Messages counter = ", self.messages_counter)
 
 
 if __name__ == "__main__":
