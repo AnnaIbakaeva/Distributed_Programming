1e0e09f4350c9f24037725f41b71743af257e4a0
Анна Ибакаева <annaibakaeva@gmail.com>
2016-11-05 09:12:37 +0500

    453534

--- client2.py
+++ client2.py
@@ -1,5 +1,6 @@
 import rpyc
 from copy import deepcopy
+import time
 
 
 class Client(object):
@@ -9,7 +10,6 @@ class Client(object):
         self.name = "Kate"
         self.iterationsCount = 100000
         self.iterations_size = 10
-        self.active_clients_count = 0
         self.isStart = False
         self.step = 0
         self.other_last_iteration = 0
@@ -20,6 +20,7 @@ class Client(object):
         self.current_iteration = 0
 
         self.on_connect()
+        self.active_clients_count = self.user.get_active_clients_count()
 
         self.update_iterations(self.iterationsCount)
         if self.current_iteration == 0:
@@ -66,6 +67,8 @@ class Client(object):
         self.isStart = True
 
         print("I am start!")
+        time.clock()
+        # print("Start time = ", time.clock())
 
         while deepcopy(self.iterationsCount) - deepcopy(self.current_iteration) > 0:
             self.calculate_pi()
@@ -84,6 +87,7 @@ class Client(object):
         result_pi += self.received_pi
         print("Result pi = ", result_pi)
         print("Result 4*pi = ", result_pi*4)
+        print("\nEnd time = ", time.clock())
 
     def calculate_pi(self):
         j = 0
@@ -98,6 +102,8 @@ class Client(object):
         self.current_iteration += self.step * j
 
     def on_send(self):
+        if len(self.my_pi) == 0:
+            return
         send_pi = 0
         for p in self.my_pi:
             send_pi += p
@@ -125,13 +131,14 @@ class Client(object):
             self.active_clients_count = last_active_clients_count
             print("Iterations count = ", self.iterationsCount)
             self.user.update_data(self.iterationsCount, self.iterations_size, self.current_iteration)
-            if self.other_last_iteration < self.current_iteration or self.step == 0:
+
+        if self.other_last_iteration < self.current_iteration or self.step == 0:
                 self.step = self.size
+                print("")
+                print("self.step = self.size ", self.step)
         else:
-            self.step = self.size
-
-
-
+            print("")
+            print("I not update step")
 
     def update_data(self, name, iterCount, iterSize, currentIteration):
         if self.name == name:
@@ -140,6 +147,11 @@ class Client(object):
         if currentIteration < self.current_iteration:
             print(" I AM NOT WILL UPDATE MY DATA!!!!")
             return
+        else:
+            self.my_pi = []
+            print("")
+            print("self.my_pi = [] !!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            print("")
 
         print("")
         print("I update my data ", iterCount, iterSize, currentIteration)
